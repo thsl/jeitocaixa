5275a97bc3e4a7a74c07580a5471077c870573db
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2018-07-21 18:37:55 -0300

    Carga inicial

---
+++ assets/js/theme.js
@@ -0,0 +1,39 @@
+// jQuery: collapse navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 900, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// inicia o wow - gerenciar as animações no scroll
+new WOW().init();
+
+// inicia o rellax - efeitos paralax
+/*var rellax = new Rellax('.rellax', {
+    center: true
+});*/
+
+//jQuery para a página rolar suavemente (requer jquerye-asing)
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
