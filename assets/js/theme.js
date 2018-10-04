bbbb09c13aa5c480609a8d095ded2fb17c8ac6a0
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2018-10-04 13:05:35 -0300

    Jeito caixa

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,13 @@
 // jQuery: collapse navbar on scroll
-$(window).scroll(function() {
+
+/*$(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
-
+*/
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
